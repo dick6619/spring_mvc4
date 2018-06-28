b01b01e617363af1cabea086a819a33ea5fec4ef
Lin <dick6619@gmail.com>
2018-06-28 23:52:05 +0800

    更新

--- spring-mvc4-mybatis/src/main/java/com/iii/emp/batch/job001/MainTest.java
+++ spring-mvc4-mybatis/src/main/java/com/iii/emp/batch/job001/MainTest.java
@@ -12,68 +12,80 @@ import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 public class MainTest {
 	public static void main(String[] args) throws Exception {
 		
-		/**jobDetail*/
+		// jobDetailFactory
 		JobDetailFactoryBean jobDetailFactory = new JobDetailFactoryBean();
+		
+		// ~~~~~~~~~~~~~~~~~ create job ~~~~~~~~~~~~~~~~~~~~
 		// name can not empty
 		jobDetailFactory.setName("job");
 		jobDetailFactory.setJobClass(QuartzJobSample1.class);
+		// 可以放job內屬性成員的name， 但要有set方法注入。無法直接使用spring-autowired
+		jobDetailFactory.setJobDataMap(new JobDataMap());
 		jobDetailFactory.setDurability(true);
 		// setJobDetail
 		jobDetailFactory.afterPropertiesSet();
 		// getJobDetail by factory
 		JobDetail jobDetail = jobDetailFactory.getObject();
 		
-		//
-		jobDetailFactory = new JobDetailFactoryBean();
+		// ~~~~~~~~~~~~~~~~~ create job1 ~~~~~~~~~~~~~~~~~~~~
 		// name can not empty
 		jobDetailFactory.setName("job1");
 		jobDetailFactory.setJobClass(QuartzJobSample2.class);
+		// 可以放job內屬性成員的name， 但要有set方法注入。無法直接使用spring-autowired
+		jobDetailFactory.setJobDataMap(new JobDataMap());
 		jobDetailFactory.setDurability(true);
 		// setJobDetail
 		jobDetailFactory.afterPropertiesSet();
 		// getJobDetail by factory
 		JobDetail jobDetail1 = jobDetailFactory.getObject();
 		
-		/**CronTrigger*/
+		// CronTrigger
 		CronTriggerFactoryBean triggerFactory = new CronTriggerFactoryBean();
+		
+		// ~~~~~~~~~~~ create trigger and bind jobDetail ~~~~~~~~~~~~
 		// name can not empty
 		triggerFactory.setName("trigger");
 		triggerFactory.setJobDetail(jobDetail);
 		triggerFactory.setCronExpression("0/3 * * * * ?");
+		// 會把jobDetail給放進來
+		triggerFactory.setJobDataMap(new JobDataMap());
 		// setCronTrigger
 		triggerFactory.afterPropertiesSet();
 		// getCronTrigger
 		Trigger trigger = triggerFactory.getObject();
-		// 本以為不用重new直接在set就好．但trigger,及trigger1內的jobDataMap不處理set的話都參照同一個obj．在全域變數有先new出
-		// 其餘的屬性變數沒有初始值必須set．原先的指定值雖在重新set後且指定給trigger1時．還是有被trigger參照．
-		triggerFactory = new CronTriggerFactoryBean();
+		
+		// ~~~~~~~~~~ create trigger1 and bind jobDetail1 ~~~~~~~~~~~
 		// name can not empty
-//		triggerFactory.setJobDataMap(new JobDataMap());
 		triggerFactory.setName("trigger1");
 		triggerFactory.setJobDetail(jobDetail1);
 		triggerFactory.setCronExpression("0/10 * * * * ?");
+		// 會把jobDetail給放進來
+		triggerFactory.setJobDataMap(new JobDataMap());
 		// setCronTrigger
 		triggerFactory.afterPropertiesSet();
 		// getCronTrigger
 		Trigger trigger1 = triggerFactory.getObject();
-		// ....... 這下面能發現jobDataMap參照同一個址，導致jobKey不一致
+		
+		
+		// ~~~~~~~~~~~~ 測試
+		// jobDataMap如果沒set的話，同個factory產出的trigger內的map則參照同一個址，導致jobKey不一致
+		// trigger的jobKey要跟detail的jobKey一致否則噴錯 
 		System.out.println(trigger == trigger1);
 		System.out.println(trigger.getJobDataMap() == trigger1.getJobDataMap());
 		System.out.println(trigger.getJobKey() == trigger1.getJobKey());
+		// 錯的，jobKey被後者蓋過
 		JobDetail jobDetailInMap = (JobDetail) trigger.getJobDataMap().get("jobDetail");
-		// 錯的，變成新的
 		System.out.println("jobDetail key = " + jobDetailInMap.getKey());
 		// 正確的
 		System.out.println("trigger key = " + trigger.getJobKey());
 		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-		//
+		
+		// schedulerFactory
 		SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
 		schedulerFactoryBean.setTriggers(trigger, trigger1);
 		schedulerFactoryBean.setOverwriteExistingJobs(true);
 		schedulerFactoryBean.afterPropertiesSet();
 		schedulerFactoryBean.start();
-//		Scheduler scheduler = schedulerFactoryBean.getObject();
-//		scheduler.start();
 
 		// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	}
