38ec29d5954f5fecf85c64bec63124e64c62d1b2
cslin19 <dick6619@gmail.com>
2022-10-18 19:47:58 +0800

    Delete spring-mvc4-jpa2 directory

--- spring-mvc4-jpa2/src/main/java/com/iii/emp/rpt/service/EmpRptService.java
+++
@@ -1,5 +0,0 @@
-package com.iii.emp.rpt.service;
-
-public interface EmpRptService {
-
-}
--- spring-mvc4-jpa2/src/main/java/com/iii/emp/rpt/service/impl/EmpRptServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.iii.emp.rpt.service.impl;
-
-import com.iii.emp.rpt.service.EmpRptService;
-
-public class EmpRptServiceImpl implements EmpRptService {
-
-}
